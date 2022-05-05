0713e02e8ca10065a188ad03f47700eba3725b6d
Ainur Sabitov <54450362+oinuritto@users.noreply.github.com>
2022-05-05 21:14:26 +0300

    Delete src/ru/kpfu/itis/oinuritto directory

--- src/ru/kpfu/itis/oinuritto/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.itis.oinuritto;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    new App();
-    }
-}
--- src/ru/kpfu/itis/oinuritto/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package ru.kpfu.itis.oinuritto.commands;
-
-import ru.kpfu.itis.oinuritto.FileDownloader;
-import java.util.ArrayList;
-
-public interface Command {
-    void execute(ArrayList<FileDownloader> fileDownloadersList, ArrayList<Thread> threadsList);
-}
